d424662f4f8904b5a4a45b3adfd59634b2bcc7f0
eachoat <eachoat@gmail.com>
2023-12-09 10:43:00 -0500

    12/2 Homework

--- newNonPlayableCharacter.js
+++ newNonPlayableCharacter.js
@@ -1,51 +1,92 @@
 function newNonPlayableCharacter(x, y) {
-    let element = newImage('assets/red-character/static.gif')
+    let element = newImage('assets/red-character/static.gif');
     element.style.zIndex = 1;
-    
+
     let direction = null;
+    let intervalId; // To store the interval reference
 
     function moveCharacter() {
         if (direction === 'west') {
-            x -= 1
+            x -= 1;
         }
         if (direction === 'north') {
-            y += 1
+            y += 1;
         }
         if (direction === 'east') {
-            x += 1
+            x += 1;
         }
         if (direction === 'south') {
-            y -= 1
+            y -= 1;
         }
-        element.style.left = x + 'px'
-        element.style.bottom = y + 'px'
+        element.style.left = x + 'px';
+        element.style.bottom = y + 'px';
     }
 
-    setInterval(moveCharacter, 1)
-
-    function walkEast() {
-        direction = 'east'
-        element.src = `./assets/red-character/east.gif`
+    function startMovingCharacter() {
+        intervalId = setInterval(moveCharacter, 1);
     }
 
-    function walkNorth() {
-        direction = 'north'
-        element.src = `./assets/red-character/north.gif`
+    function stopMovingCharacter() {
+        clearInterval(intervalId);
     }
 
-    function walkWest() {
-        direction = 'west'
-        element.src = `./assets/red-character/west.gif`
+    async function walkEast(npc) {
+        direction = 'east';
+        element.src = `./assets/red-character/east.gif`;
+        await sleep(2000);
+        stopMovingCharacter();
+        npc.walkSouth(npc);
     }
 
-    function walkSouth() {
-        direction = 'south'
-        element.src = `./assets/red-character/south.gif`
+    async function walkNorth(npc) {
+        direction = 'north';
+        element.src = `./assets/red-character/north.gif`;
+        await sleep(2000);
+        stopMovingCharacter();
+        npc.walkNorth(npc);
     }
+
+    async function walkWest(npc) {
+        direction = 'west';
+        element.src = `./assets/red-character/west.gif`;
+        await sleep(2000);
+        stopMovingCharacter();
+        npc.walkWest(npc);
+    }
+
+    const pc = newPlayableCharacter (100, 110)
+
+    const npc = newNonPlayableCharacter(50, 300)
+
+    async function walkSouth(npc) {
+        direction = 'south';
+        element.src = `./assets/red-character/south.gif`;
+        await sleep(2000);
+        stopMovingCharacter();
+        npc.walkSouth(npc);
+    }
+
+    async function moveNPC(){
+        await npc.walkNorth(1400)
+        await npc.walkEast(1200)
+        await npc.walkSouth(300)
+        await npc.walkEast(1500)
+        await npc.walkSouth(1500)
+        await npc.walkWest(2700)
+        await npc.walkNorth(400)
+    }
+    
+    moveNPC ()
 
     function stop() {
-        direction = null
-        element.src = `./assets/red-character/static.gif`
+        direction = null;
+        element.src = `./assets/red-character/static.gif`;
+    }
+
+    function sleep(time) {
+        return new Promise(resolve => {
+            setTimeout(resolve, time);
+        });
     }
 
     return {
@@ -54,6 +95,7 @@ function newNonPlayableCharacter(x, y) {
         walkNorth: walkNorth,
         walkEast: walkEast,
         walkSouth: walkSouth,
-        stop: stop
-    }
+        stop: stop,
+        executePath: executePath
+    };
 }
